8103e9615c90e7055a0166d7b395dd038e0c2117
Nisheshchy <nisheshchy2022@gmail.com>
2026-03-07 22:06:44 +0545

    feat: implement main routing and navigation structure

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
 /** @format */
 
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./Components/Home";
 import About from "./Components/About";
 import Work from "./Components/Work";
 import Testimonial from "./Components/Testimonial";
 import Contact from "./Components/Contact";
+import Product from "./Components/Product";
 import Footer from "./Components/Footer";
+import FoodMenu from "./Components/FoodMenu";
+import Navbar from "./Components/Navbar";
+import ContactUs from "./Components/ContactUs";
+import TestimonialsPage from "./Components/TestimonialsPage";
+import AboutUs from "./Components/AboutUs";
 
 function App() {
   return (
-    <div className="App">
-      <Home />
-      <About />
-      <Work />
-      <Testimonial />
-      <Contact />
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <Home />
+                <About />
+                <Work />
+                <Product />
+                <Testimonial />
+                <Contact />
+              </>
+            }
+          />
+          <Route path="/menu" element={<FoodMenu />} />
+          <Route path="/about" element={<AboutUs />} />
+          <Route path="/contact" element={<ContactUs />} />
+          <Route path="/testimonials" element={<TestimonialsPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
