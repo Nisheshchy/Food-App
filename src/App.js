0baab4de58979b29d21c2ad5674f2be4fd101c06
Nisheshchy <nisheshchy2022@gmail.com>
2026-03-08 00:10:12 +0545

    feat: Add Cart route and global provider

--- src/App.js
+++ src/App.js
@@ -14,34 +14,41 @@ import Navbar from "./Components/Navbar";
 import ContactUs from "./Components/ContactUs";
 import TestimonialsPage from "./Components/TestimonialsPage";
 import AboutUs from "./Components/AboutUs";
+import Cart from "./Components/Cart";
+import { CartProvider } from "./Context/CartContext";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route
-            path="/"
-            element={
-              <>
-                <Home />
-                <About />
-                <Work />
-                <Product />
-                <Testimonial />
-                <Contact />
-              </>
-            }
-          />
-          <Route path="/menu" element={<FoodMenu />} />
-          <Route path="/about" element={<AboutUs />} />
-          <Route path="/contact" element={<ContactUs />} />
-          <Route path="/testimonials" element={<TestimonialsPage />} />
-        </Routes>
-        <Footer />
-      </div>
-    </Router>
+    <CartProvider>
+      <Router>
+        <div className="App">
+          <div className="content-wrapper">
+            <Navbar />
+            <Routes>
+              <Route
+                path="/"
+                element={
+                  <>
+                    <Home />
+                    <About />
+                    <Work />
+                    <Product />
+                    <Testimonial />
+                    <Contact />
+                  </>
+                }
+              />
+              <Route path="/menu" element={<FoodMenu />} />
+              <Route path="/about" element={<AboutUs />} />
+              <Route path="/contact" element={<ContactUs />} />
+              <Route path="/testimonials" element={<TestimonialsPage />} />
+              <Route path="/cart" element={<Cart />} />
+            </Routes>
+          </div>
+          <Footer />
+        </div>
+      </Router>
+    </CartProvider>
   );
 }
 
