13f9cd4aa0c618da986d029f80a9f7a1100eec76
Nisheshchy <nisheshchy2022@gmail.com>
2026-03-07 21:34:26 +0545

    update code and fix error

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,13 +3,10 @@
 import React from "react";
 import BannerBackground from "../Assets/home-banner-background.png";
 import BannerImage from "../Assets/home-banner-image.png";
-import Navbar from "./Navbar";
 import { FiArrowRight } from "react-icons/fi";
-
 const Home = () => {
   return (
-    <div className="home-container">
-      <Navbar />
+    <div id="home" className="home-container">
       <div className="home-banner-container">
         <div className="home-bannerImage-container">
           <img src={BannerBackground} alt="" />
